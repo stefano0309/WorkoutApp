18f8aba37ed79b9aba8ea30a026964b2f3fe2dbb
sdutto3981-commits <s.dutto.3981@vallauri.edu>
2026-09-03 09:33:34 +0200

    testReleasev1.0.1

--- android/app/src/main/java/com/example/app/HybridTrainingWidgetProvider.java
+++ android/app/src/main/java/com/example/app/HybridTrainingWidgetProvider.java
@@ -1,5 +1,7 @@
 package com.example.app;
 
+import com.stefano.workout.R;
+
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
