f30901df8397ac95058d5494100b9a867b425f2b
Stefano Dutto <117385811+stefano0309@users.noreply.github.com>
2026-09-04 17:35:46 +0200

    test: cover weekly photo lifecycle receiver constants

---
+++ android/app/src/test/java/com/example/app/WeeklyPhotoReceiverTest.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Intent;
+
+import org.junit.Test;
+
+public class WeeklyPhotoReceiverTest {
+    @Test
+    public void bootActionsAreSystemLifecycleActions() {
+        assertEquals("android.intent.action.BOOT_COMPLETED", Intent.ACTION_BOOT_COMPLETED);
+        assertEquals("android.intent.action.TIME_CHANGED", Intent.ACTION_TIME_CHANGED);
+        assertEquals("android.intent.action.TIMEZONE_CHANGED", Intent.ACTION_TIMEZONE_CHANGED);
+    }
+}
